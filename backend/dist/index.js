d09c00eff218e44c19e996f83e8e23f6ac44a9be
Trzneaoo <fussball05211009@gmail.com>
2026-05-31 00:35:55 +0900

    feat: backend/frontend の初期構成を追加（Express+TypeScript, React+Vite） - dotenv を導入し、開発時の環境変数読み込みを設定

--- backend/dist/index.js
+++ backend/dist/index.js
@@ -1,5 +1,20 @@
 "use strict";
-console.log("Hello World");
-const num1 = 10;
-const num2 = 20;
-console.log(num1 + num2);
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const app = (0, express_1.default)();
+app.get("/", (req, res) => {
+    res.json({
+        message: "Welcome to Productify Store API - Powered by PostgreSQL, Drizzle ORM, and Clerk Authentication",
+        endpoints: {
+            users: "/api/users",
+            products: "/api/products",
+            comments: "/api/comments",
+        }
+    });
+});
+app.listen(3000, () => {
+    console.log("Server is running on port 3000");
+});
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,11 +1,19 @@
 import express from "express";
+import { ENV } from "./config/env";
 
 const app = express();
 
 app.get("/", (req, res) => {
-    res.json({ message: "Hello World" });
+    res.json({ 
+        message: "Welcome to Productify Store API - Powered by PostgreSQL, Drizzle ORM, and Clerk Authentication", 
+        endpoints:{
+            users: "/api/users",
+            products: "/api/products",
+            comments: "/api/comments",
+        }
+    });
 });
 
-app.listen(3000, () => {
-    console.log("Server is running on port 3000");
+app.listen(ENV.PORT, () => {
+    console.log(`Server is running on port ${ENV.PORT}`);
 });
